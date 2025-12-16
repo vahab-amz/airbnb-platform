603e9ef16c655e79bc41fa42320e968e2a5cbed9
VAHAB <vahab.azimzadeh@gmail.com>
2025-12-16 15:49:18 +0100

    Add nextInt for multiLanguage

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,8 @@
+import { useTranslations } from 'next-intl';
+
+export default function HomePage() {
+    const t = useTranslations('HomePage');
+    return (
+      <h1>{t('title')}</h1>
+    );
+}
